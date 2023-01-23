1989df18f7a4309d56e33c2f80387d762b03afad
Lorenzo G <lorenzo.gurgo@gmail.com>
2023-01-23 14:39:47 +0100

    Update TrouveTonJob.py

--- TrouveTonJob.py
+++ TrouveTonJob.py
@@ -4,7 +4,6 @@ import pandas as pd
 url = "https://raw.githubusercontent.com/Lorenzo1208/Projet_TrouveTonJob/main/data.json"
 response = requests.get(url)
 data = response.json()
-
 df = pd.DataFrame(data)
 
 print(df)
