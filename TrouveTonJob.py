161b6d883246c9df35e83e887c18851e4c68630b
Adrien Riguet <adrien.riguet@hotmail.com>
2023-01-24 13:57:35 +0100

    Salaire min/max + nom soc + date

--- TrouveTonJob.py
+++ TrouveTonJob.py
@@ -1,9 +1,29 @@
-import requests
+import string
 import pandas as pd
+import numpy as np
+import requests
 
 url = "https://raw.githubusercontent.com/Lorenzo1208/Projet_TrouveTonJob/main/data.json"
 response = requests.get(url)
 data = response.json()
 df = pd.DataFrame(data)
 
-print(df)
+liste_salaires = df['lieu'].apply(lambda x : x[1] if len(x) > 1 else '')
+
+df['lieu'] = df['lieu'].apply(lambda x : x[0])
+df['Nom de la société'] = df['Type de poste'].apply(lambda x : x[2] if len(x) > 2 else '')
+df['Type de contrat'] = df['Type de poste'].apply(lambda x : x[7].split(' - ')[0] if len(x) > 7 else '')
+df.drop(columns='Type de poste', inplace=True)
+
+df = df.apply(lambda c : c.apply(lambda x : ','.join([s.strip("\n ").lower() for s in (x.split(',') if type(x) != list else x)])))
+
+liste_dates = df['Date de publication'].apply(lambda x: '0 j' if x.find('heures') != -1 else x.removeprefix('postée il y a ').replace('postée hier', '1 j').strip('iours'))
+
+df['Date de publication'] = liste_dates.apply(lambda x : pd.to_datetime("2023-01-15") - pd.DateOffset(months=int(x.split()[0])) if x.split()[1] == 'm' else pd.to_datetime("2023-01-15") - pd.DateOffset(days=int(x.split()[0])))
+df['Date de publication'] = pd.to_datetime(df['Date de publication'])
+
+df['Salaire minimum'] = liste_salaires.apply(lambda x : float(x.split(' - ')[0].strip(' €\n').replace('.', '').replace(',', '.')) if x != '' else np.nan)
+df['Salaire maximum'] = liste_salaires.apply(lambda x : float(x.split(' - ')[1].strip(' €/an\n' + string.ascii_letters).replace('.', '').replace(',', '.')) if x != '' else np.nan)
+
+#print(dates)
+df.to_csv("data_test1.csv")
