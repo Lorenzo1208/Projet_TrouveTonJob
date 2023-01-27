108103b8921080333b765e4ada1f33426b5ff0e5
dridroy <adri1.riguet@hotmail.fr>
2023-01-27 17:45:10 +0100

    tests models

--- modelisation.py
+++ modelisation.py
@@ -1,7 +1,7 @@
 from sklearn.compose import ColumnTransformer
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.impute import SimpleImputer
-from sklearn.linear_model import SGDRegressor
+from sklearn.linear_model import SGDRegressor, LinearRegression, Ridge, Lasso, ElasticNet
 from sklearn.metrics import r2_score
 from sklearn.model_selection import train_test_split
 from sklearn.multioutput import MultiOutputRegressor
@@ -9,10 +9,17 @@ from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import MinMaxScaler, OneHotEncoder
 
 from data_cleaning import *
+import time
 
-df = get_dataset_1()
+start_time = time.time()
 
+df = get_dataset_3().drop(columns='origine')
+
+df.dropna(inplace=True)
+df = df[df['Salaire minimum'] < 300000]
 df.dropna(subset=['Salaire minimum', 'Salaire maximum'], inplace=True)
+
+df['competences'] = df['competences'].apply(lambda x : x.replace(', ', ' ').replace(',', ' '))
 
 df['Date de publication'] = df['Date de publication'].values.astype(np.int64)
 
@@ -20,7 +27,7 @@ X = df.drop(['Salaire minimum', 'Salaire maximum'], axis=1)
 y = df[['Salaire minimum', 'Salaire maximum']]
 
 # J'ai pas mis de simple imputer parce que ça me parait pas logique de mettre un nom de société au pif quand il y en a pas
-column_cat = ['lieu', 'Nom de la société', 'Type de contrat', 'Intitulé du poste']
+column_cat = ['lieu', 'Nom de la société', 'Type de contrat']
 transfo_cat = Pipeline([
     ('onehot', OneHotEncoder(handle_unknown='ignore', sparse_output = False))
 ])
@@ -38,21 +45,54 @@ transfo_text = Pipeline([
 preparation = ColumnTransformer([
         ('data_cat', transfo_cat , column_cat),
         ('data_num', transfo_num , column_num),
-        ('data_track', transfo_text , 'competences')
+        ('data_comp', transfo_text , 'competences'),
+        ('data_poste', transfo_text , 'Intitulé du poste')
+
 ])
 
-model = MultiOutputRegressor(SGDRegressor(max_iter=100000))
+models = {
+    'SGDRegressor': SGDRegressor(max_iter=100000),
+    'LinearRegression': LinearRegression(),
+    'Ridge': Ridge(max_iter=100000),
+    'Lasso': Lasso(max_iter=100000),
+    'ElasticNet': ElasticNet(max_iter=100000)
+}
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=10)
+scores = {}
+best = -100
+best_random_state = 0
+best_model = ''
 
-print(f"X_train = {X_train.shape}\nX_test = {X_test.shape}")
+for name_model in models:
 
-pipe_model = Pipeline([('preparation', preparation),
-                        ('model',model)])
+    scores[name_model] = []
 
-pipe_model.fit(X_train, y_train)
+    for random_state in range(1, 100):
 
-y_pred = pipe_model.predict(X_test)
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=random_state)
 
-score = r2_score(y_test, y_pred)
-print("score :", round(score, 5))
+        print(f"X_train = {X_train.shape} - X_test = {X_test.shape} - model = {name_model} - random_state = {random_state}", end='')
+
+        model = MultiOutputRegressor(models[name_model])
+        pipe_model = Pipeline([('preparation', preparation), ('model',model)])
+
+        pipe_model.fit(X_train, y_train)
+
+        y_pred = pipe_model.predict(X_test)
+
+        score = r2_score(y_test, y_pred)
+
+        scores[name_model].append(score)
+
+        best_model = name_model if best < score else best_model
+        best_random_state = random_state if best < score else best_random_state
+        best = score if best < score else best
+
+        print(" - score => ", round(score, 5), end='\n\n')
+
+print(f"Best = {best} - Best random_state = {best_random_state} - Best model = {best_model}", end='\n\n')
+print('\n'.join([f" - {n:20} moy : {sum(scores[n]) / len(scores[n])}" for n in models]))
+
+duree = time.time() - start_time
+
+print(f"{int(duree/3600)} h {int((duree%3600)/60)} m {duree%3600%60}")
