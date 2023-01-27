190b40d23d94f6dfa5a2839b3251afef575681d8
dridroy <adri1.riguet@hotmail.fr>
2023-01-27 14:46:14 +0100

    néttoyage + datasets

--- data_cleaning.py
+++ data_cleaning.py
@@ -12,6 +12,7 @@ locale.setlocale(locale.LC_ALL, 'fr_FR.UTF-8')
 
 URL = "https://raw.githubusercontent.com/Lorenzo1208/Projet_TrouveTonJob/main/data.json"
 
+
 def try_download_json(url:str) -> pd.DataFrame:
 
     try:
@@ -24,7 +25,17 @@ def try_download_json(url:str) -> pd.DataFrame:
         quit()
 
 
-def clean_data(df:pd.DataFrame) -> pd.DataFrame:
+def try_read_csv(file) -> pd.DataFrame:
+
+    try:
+        return pd.read_csv(file, index_col=0)
+
+    except:
+        print(f"Erreur de lecture du csv - {file}")
+        quit()
+
+
+def clean_data_1(df:pd.DataFrame) -> pd.DataFrame:
 
     liste_salaires = df['lieu'].apply(lambda x : x[1] if len(x) > 1 else '')
 
@@ -72,24 +83,12 @@ def clean_data(df:pd.DataFrame) -> pd.DataFrame:
     return df
 
 
-# Tarik lignes 66 à .... réservées au nettoyage du fichier data_scrapping.csv #########################################################
-def try_read_csv(file) -> pd.DataFrame:
-
-    try:
-        df2 = pd.read_csv(file, index_col=0)
-        return df2
-    except:
-        print(f"Erreur de lecture du csv - {file}")
-        quit()
-
-
 def clean_data_scrapping(df:pd.DataFrame) -> pd.DataFrame:
 
-    ############################## Salaires ##############################################################################################################################
+    ##### Salaires #####
     df[['Salaire minimum', 'Salaire maximum']] = df.loc[df['Salaires'].str.count('\d')>3,'Salaires'].str.extract(r'(\d+[,.]\d+|\d+).*?(\d+[,.]\d+|\d+)', expand=True)
-
-
     df[['Salaire minimum', 'Salaire maximum']] = df[['Salaire minimum', 'Salaire maximum']].apply(lambda x: x.str.replace(',', '.').astype(float))
+
     # les salaires avec k€ 
     df['Salaire minimum'] = df['Salaire minimum'].apply(lambda x: x*1000 if len(str(x).split(".")[0]) == 2 else x)
     df['Salaire maximum'] = df['Salaire maximum'].apply(lambda x: x*1000 if len(str(x).split(".")[0]) == 2 else x)
@@ -97,18 +96,17 @@ def clean_data_scrapping(df:pd.DataFrame) -> pd.DataFrame:
     df['Salaire minimum'] = df['Salaire minimum'].apply(lambda x: x*12 if len(str(x).split(".")[0]) == 4 else x)
     df['Salaire maximum'] = df['Salaire maximum'].apply(lambda x: x*12 if len(str(x).split(".")[0]) == 4 else x)
 
-    #replace 0 with ''
     df['Salaire maximum'] = df['Salaire maximum'].apply(lambda x: None if x == 0 else x)
 
-    ###########################   Type de contrat   ##########################################################################################################################################
+    ##### Type de contrat #####
 
-    df['Type de contrat'] = df['Type de contrat'].apply(lambda x : str(x).split('-')[0].lower())
+    df['Type de contrat'] = df['Type de contrat'].apply(lambda x : str(x).split('-')[0].strip(' \u00a0\n\r').lower())
 
-    ###########################   lieu   ##########################################################################################################################################
+    ##### lieu #####
 
     df['lieu'] = df['lieu'].apply(lambda x: re.sub(r'[^a-zA-Z]', ' ', x).strip().lower())
 
-    ###########################   Intitulé de poste   ##########################################################################################################################################
+    ##### Intitulé de poste #####
 
     df['Intitulé du poste'] = df['Intitulé du poste'].apply(lambda x: re.sub(r'[hf]/[hf]|[-\/()]', '', x.lower()))
     keywords = ["stage","business analyst","analyste fonctionnel","architecte","apprenti","data ingenieur","data ingénieur", "data engineer","data analyst","data scientist","consultant","ingenieur","chef de projet","concepteur","alternance",
@@ -120,46 +118,42 @@ def clean_data_scrapping(df:pd.DataFrame) -> pd.DataFrame:
         df["Intitulé du poste"] = df["Intitulé du poste"].str.replace(rf"(.*{keyword})", keyword, regex=True)
         df["Intitulé du poste"] = df["Intitulé du poste"].str.replace(rf"({keyword}.*)", keyword, regex=True)
 
-    ########################## Date ################################################################################################
+    ##### Date #####
 
-    df['Date de publication'] = df['Date de publication'].apply(lambda x: datetime.strptime(x.removeprefix('Actualisé le ').removeprefix('Publié le '), '%d %B %Y').strftime("%Y-%m-%d"))
+    df['Date de publication'] = df['Date de publication'].apply(lambda x: datetime.strptime(x.strip(' \n\r').removeprefix('Actualisé le ').removeprefix('Publié le '), '%d %B %Y'))
 
-    ########################### Nom de la société ################################################################################
+    ##### Nom de la société #####
 
-    df['Nom de la société'] = df['Nom de la société'].str.lower()
+    df['Nom de la société'] = df['Nom de la société'].apply(lambda x : str(x).lower().strip(' \r\n'))
 
-    ########################## df dropna and drop 'Salaires' et colonne index duplicat ##############################################
+    ##### df dropna and drop 'Salaires' et colonne index duplicat #####
     df.drop('Salaires', axis=1, inplace=True)
-    df.dropna(how='any', inplace=True)
+    #df.dropna(how='any', inplace=True)
 
     return df
 
 
+def get_dataset_1():
+
+    return clean_data_1(try_download_json(URL))
+
+
+def get_dataset_scrapping():
+
+    return clean_data_scrapping(try_read_csv('data_scrapping.csv'))
 
 
 def main():
 
-    df = try_download_json(URL)
-    df = clean_data(df)
+    df1 = get_dataset_1()
 
-    df2 = try_read_csv('data_scrapping.csv')
-    df2 = clean_data_scrapping(df2)
-    # jeu de données final df1+df2
-    df = df.dropna().reset_index(drop=True)
-    
-    # Imputation à faire sur Type de poste et lieu en prenant la valeur qui revient le plus
-    df3 = pd.concat([df,df2],axis=0)
-    # Imputation Type de contrat fillna with cdi et lieu with paris
-    df3['Type de contrat'] = df3['Type de contrat'].replace('',np.nan,regex=True)
-    df3['Type de contrat'] = df3['Type de contrat'].replace(np.nan,'cdi',regex=True) # Remplace le blank avec nan
-    # df3['Type de contrat'] = df3['Type de contrat'].fillna("cdi")
-    df3['lieu'] = df3['lieu'].replace('',"paris",regex=True) # Remplace le blank avec nan
-    
-    df3 = df3.reset_index(drop=True)
-    print(df2.shape)
-    df.to_csv("data_clean.csv", index=False)
-    df2.to_csv("data_scrapping_clean.csv", index=False)
-    df3.to_csv("data_finale_clean.csv", index=False)
+    df2 = get_dataset_scrapping()
+
+    df3 = pd.concat([df1, df2])
+
+    df1.to_csv("dataset_1.csv", index=False)
+    df2.to_csv("dataset_2.csv", index=False)
+    df3.to_csv("dataset_3.csv", index=False)
 
 
 if __name__ == '__main__':
