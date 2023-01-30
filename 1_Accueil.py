dd8415a7c21ed7daad90d60588e780e9ed0af004
Lorenzo G <55632146+Lorenzo1208@users.noreply.github.com>
2023-01-30 22:57:18 +0100

    Add files via upload

---
+++ 1_Accueil.py
@@ -0,0 +1,15 @@
+import numpy as np
+import altair as alt
+import pandas as pd
+import streamlit as st
+import plotly.express as px
+import seaborn as sns
+from streamlit_option_menu import option_menu 
+from analyse import *
+
+st.set_page_config(
+    page_title="TrouveTonJob",
+    page_icon=":sunglasses:",
+)
+st.title("TrouveTonJob")
+
